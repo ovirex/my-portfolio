4ff3410a4caaf5b75054c018aabd409d3bc11edc
Ovidio Perez <oapm10@gmail.com>
2022-01-25 21:23:01 -0400

    Update about page to use content from the cms

--- components/main-components/AboutSection.vue
+++ components/main-components/AboutSection.vue
@@ -15,89 +15,13 @@
         <div class="col-md-4 ml-auto order-2" data-aos="fade-up">
           <h3 class="h3 mb-4">Skills</h3>
           <ul class="list-unstyled">
-            <li class="mb-3">
+            <li
+              v-for="(skill, index) in aboutInfo.skills"
+              :key="index"
+              class="mb-3"
+            >
               <div class="d-flex mb-1">
-                <strong>WordPress</strong>
-                <span class="ml-auto"></span>
-              </div>
-              <div class="progress custom-progress">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style="width: 100%"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-            </li>
-            <li class="mb-3">
-              <div class="d-flex mb-1">
-                <strong>Photoshop</strong>
-                <span class="ml-auto"></span>
-              </div>
-              <div class="progress custom-progress">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style="width: 100%"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-            </li>
-            <li class="mb-3">
-              <div class="d-flex mb-1">
-                <strong>HTML5/CSS3</strong>
-                <span class="ml-auto"></span>
-              </div>
-              <div class="progress custom-progress">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style="width: 100%"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-            </li>
-            <li class="mb-3">
-              <div class="d-flex mb-1">
-                <strong>Veu</strong>
-                <span class="ml-auto"></span>
-              </div>
-              <div class="progress custom-progress">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style="width: 100%"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-            </li>
-            <li class="mb-3">
-              <div class="d-flex mb-1">
-                <strong>Angular</strong>
-                <span class="ml-auto"></span>
-              </div>
-              <div class="progress custom-progress">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style="width: 100%"
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-            </li>
-            <li class="mb-3">
-              <div class="d-flex mb-1">
-                <strong>React</strong>
+                <strong>{{ skill.text }}</strong>
                 <span class="ml-auto"></span>
               </div>
               <div class="progress custom-progress">
@@ -115,31 +39,10 @@
         </div>
 
         <div class="col-md-7 mb-5 mb-md-0" data-aos="fade-up">
-          <p>
-            <img
-              src="~/assets/img/person_1_sq.jpg"
-              alt="Image"
-              class="img-fluid"
-            />
+          <p v-if="aboutInfo.hasOwnProperty('profile_pic')">
+            <img :src="aboutInfo.profile_pic" alt="Image" class="img-fluid" />
           </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            dignissimos delectus minima reprehenderit molestias illo dolorem et,
-            odio!
-          </p>
-          <p>
-            Fuga fugit distinctio delectus iure vitae consequatur excepturi,
-            mollitia, consectetur molestias sapiente rem consequuntur, illum
-            adipisci, sed obcaecati!
-          </p>
-          <p>
-            Ex, dolorem qui voluptas reprehenderit provident, ad ipsum iure a
-            consequatur voluptatem incidunt nobis. Vitae reiciendis quae ex.
-          </p>
-          <p>
-            Optio consectetur culpa nemo, fugit pariatur veniam voluptate
-            laudantium rerum fuga dolor in maiores ea nisi voluptatibus. Minus?
-          </p>
+          <nuxt-content :document="aboutInfo"></nuxt-content>
           <p>
             <a
               v-for="(resume, index) in resumesToShow"
@@ -160,15 +63,29 @@ export default {
   name: 'AboutSection',
   data() {
     return {
+      aboutInfo: {},
       resumesToShow: [],
     }
   },
   async fetch() {
-    const resumes = await this.$content('resumes')
-      .only(['resumes_list'])
-      .fetch()
+    try {
+      const resumes = await this.$content('resumes')
+        .only(['resumes_list'])
+        .fetch()
+      this.resumesToShow = resumes[0].resumes_list
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
 
-    this.resumesToShow = resumes[0].resumes_list
+    try {
+      const contentAbout = await this.$content('about_info').fetch()
+
+      this.aboutInfo = contentAbout[0]
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
   },
 }
 </script>
