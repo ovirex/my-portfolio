e6023a5c75fe2aedd623fb92140575f45b93a62e
ovirex <ovirex@users.noreply.github.com>
2022-02-02 22:48:33 +0000

    deploy: 1401314c5a3d3d7485a686ea00384bad0e2992fb

---
+++ _nuxt/static/1643842109/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontact","\u002Fabout","\u002Fworks","\u002Fworks\u002Ff1CKgRsGn6E1","\u002Fworks\u002FuILNwCKBgiqI","\u002Fworks\u002FC8IdXAWASZka","\u002Fworks\u002Fm5SD-BNSXGXE","\u002Fworks\u002FmoG_M9wpKp-E","\u002Fworks\u002FJ_rPBeMRvc0A","\u002Fworks\u002FrUFOwh9Jx6qj"]})
