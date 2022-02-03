a345b867be6278087704c61fc9d377bbc59f3e59
ovirex <ovirex@users.noreply.github.com>
2022-02-03 20:02:54 +0000

    deploy: 0341e6e9bc0c3cec48aaa1d43e803093c3c52104

--- _nuxt/static/1643842410/works/moG_M9wpKp-E/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643842410",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002FmoG_M9wpKp-E",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"17bbac6f"}}}}(null));
