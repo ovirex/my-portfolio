39bd9e9744d93cd2b9366e02e880e1d949e7a39e
ovirex <ovirex@users.noreply.github.com>
2022-06-13 18:37:56 +0000

    deploy: 98cbdb42711a48941b0a92dda794727242b37e4a

--- _nuxt/static/1643924278/works/m5SD-BNSXGXE/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643924278",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fm5SD-BNSXGXE",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2f098ef1"}}}}(null));
