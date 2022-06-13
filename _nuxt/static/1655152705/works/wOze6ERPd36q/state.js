dfaab2fb7a4f49f5a11fbca1eb0c057147c24735
ovirex <ovirex@users.noreply.github.com>
2022-06-13 20:38:29 +0000

    deploy: 625b17d81f816a4dba06e1525a9c2e7a83acc3ca

---
+++ _nuxt/static/1655152705/works/wOze6ERPd36q/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655152705",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002FwOze6ERPd36q",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"80b3561c"}}}}(null));
